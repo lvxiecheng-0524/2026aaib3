a672e71d3c31f246c1c3c7ba9dcf29b394584bc1
lvxiecheng-0524 <14751095@me.mcu.edu.tw>
2026-09-24 09:00:06 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,12 @@
+///week02-1.cpp 今天的主題 C++ string 字串
+///File-Save As .cpp檔
+#include <iostream>
+#include <string>
+int main()
+{
+        std::cout << "What is your name?";
+        std::string name;
+        std::cin >> name;
+        std::cout << name <<"Hello! Today we're going to learn about strings.";
+
+}
---
+++ week02/week02-2.cpp
@@ -0,0 +1,12 @@
+///week02-2.cpp 要使用“命名空間”
+#include <iostream>
+#include <string>
+using namespace std;///使用std“命名空間”
+int main()
+{
+        cout << "What is your name?";
+        string name;
+        cin >> name;
+        cout << name <<"Hello! Today we're going to learn about 'namespace'.";
+
+}
