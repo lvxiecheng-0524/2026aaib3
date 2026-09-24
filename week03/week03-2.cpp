cdc5b49b09117a19a4b67a3e52aaf46111eb89c3
lvxiecheng-0524 <14751095@me.mcu.edu.tw>
2026-09-24 11:00:41 +0800

    week03

---
+++ week03/week03-2.cpp
@@ -0,0 +1,29 @@
+// week03-2.cpp 學習計劃 Basic 第8題
+// 1822.Sign of the Product of an Array
+class Solution
+{
+public:
+    int arraySign(vector<int>& nums)
+    {
+        int neg = 0; // 統計有幾個負數
+        for (int num : nums)
+        {
+            if (num < 0)
+                neg++; // 遇到負數
+            if (num == 0)
+                return 0; // 遇到0
+        }
+        if (neg % 2 == 0)
+            return 1;
+        else
+            return -1;
+        // 用錯誤的方法寫一次數字有1000個，乘起來太大了，不行（）
+        // int ans = 1;
+        //  for(int num:nums){//進階c++迴圈，逐一處理
+        //     ans *= num;//乘進去
+        // }
+        // if(ans>0) return 1;
+        // if(ans<0) return -1;
+        // return 0;
+    }
+};
